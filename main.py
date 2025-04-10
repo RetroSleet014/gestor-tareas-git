6c48d7ceee3d583d2ef8d2e0d84ab357b01be38b
Andres <andres_sercommex@hotmail.com>
2025-04-09 18:02:34 -0600

    Version 1:Agregar tareas

--- main.py
+++ main.py
@@ -2,5 +2,12 @@
 def main():
     print("¡Bienvenido al Gestor de Tareas!")
 
+def guardar_tarea():
+    with open("tareas.txt", "a") as archivo:
+        tarea = input("Escribe una nueva tarea: ")
+        archivo.write(tarea + "\n")
+        print("Tarea guardada.\n")
+
 if __name__ == "__main__":
     main()
+
